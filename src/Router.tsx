254a12de28a8c753f48cc025dfe3187d06b159b6
stoicelephant <jamesgiuffre3@gmail.com>
2026-02-12 12:12:10 -0500

    mapbox

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from "./pages/Home/index.tsx";
 import Login from "./pages/Login/index.tsx";
 import Signup from "./pages/Signup/index.tsx";
+import MapBox from "./pages/MapBox/index.tsx";
 import CreatePost from "./pages/CreatePost/index.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 //import InformationCenter from "./Tracker.js";
@@ -16,6 +17,7 @@ return (
       <Route path="/home" element={<Homepage/>} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/create-trip" element={<CreatePost/>} />
+      <Route path="/mapbox" element={<MapBox/>} />
 </Routes>
 </AuthProvider>
 )
